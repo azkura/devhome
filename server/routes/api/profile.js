8f6c0e87c6788119855b3475158d4498554171f8
github_username <azizkoura@hotmail.com>
2019-12-19 17:25:46 +0100

    get user profile by id

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -121,4 +121,24 @@ router.get('/', async (req, res) => {
   }
 });
 
+// @route  GET api/profile/user/:user_id
+// desc    GET profile by user id
+//@access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(500).json({ msg: 'Profile not found' });
+    res.send(profile);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == 'ObjectId') {
+      return res.status(500).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
